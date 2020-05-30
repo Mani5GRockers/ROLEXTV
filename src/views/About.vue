d870770aa0db4b8f6a6ec0f7d89ff6d4a269820b
Mani5GRockers <66015149+Mani5GRockers@users.noreply.github.com>
2020-05-30 18:22:43 +0530

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -7,7 +7,7 @@
         <p>RX100 created by Mani5GRockers</p>
 		 <p>explore & share and play media of files online</p>
         <p>
-          RX100 Torrent is open source
+          RX100 Torrent open sources
         </p>
 		<p>
           <br />Visit the github repository from
