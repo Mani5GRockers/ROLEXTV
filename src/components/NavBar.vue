9df8f75ba51d6b7c7fd47446861698c0c8153ebc
Mani5GRockers <66015149+Mani5GRockers@users.noreply.github.com>
2020-06-14 18:17:55 +0530

    Update NavBar.vue

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -95,10 +95,7 @@ export default {
       drawer: true,
       mini: false,
       items: [
-        { title: "Home", icon: "fas fa-home", path: "/" },
-        { title: "Movies", icon: "fas fa-film", path: "/movies" },
-        { title: "Bookmarks", icon: "fas fa-bookmark", path: "/bookmarks" },
-        { title: "About", icon: "info", path: "/about" }
+       
       ]
     };
   },
