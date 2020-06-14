e363474fe8856a74d849438b9e5c3b1bc0d689be
Mani5GRockers <66015149+Mani5GRockers@users.noreply.github.com>
2020-06-14 17:52:34 +0530

    Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,197 +12,20 @@
           >
             <span style="color: #d21414">
               <i class="fas fa-circle logo"></i>
-              RX100
+              Access denied
             </span>
             Mani5GRockers
           </h1>
         </v-flex>
-        <v-flex xs10 offset-xs1>
-          <v-text-field
-            v-model="query"
-            label="RX100 Looking for something or you have a torrentID"
-            @keydown.enter="search"
-            :error-messages="errors"
-            :disabled="loading"
-            solo
-            light
-            :loading="loading"
-            persistent-hint
-            hint="torrentID can be torrent magnet, torrent HTTP/HTTPS url or torrent info hash"
-            clearable
-            :autofocus="!$vuetify.breakpoint.xsOnly"
-          >
-            <template v-slot:append>
-              <v-btn icon @click="loadDemo" v-if="!query">
-                <v-icon color="purple">fas fa-vial</v-icon>
-              </v-btn>
-              <v-btn icon @click="uploadTorrent" v-if="!query">
-                <v-icon color="green">fas fa-upload</v-icon>
-              </v-btn>
-              <v-btn icon @click="search">
-                <v-icon color="teal" v-if="validateQuery.isTorrentId"
-                  >fas fa-eye</v-icon
-                >
-                <v-icon color="blue" v-else>fas fa-search</v-icon>
-              </v-btn>
+       
             </template>
           </v-text-field>
-        </v-flex>
-
-        <v-flex class="text-xs-center mt-5">
-          <v-btn
-            color="gray"
-            href="/"
-            target="blank"
-          >
-            <v-icon left>fab fa-github</v-icon>Github
-          </v-btn>
-          <v-btn
-            color="gray"
-            href="/"
-            target="blank"
-          >
-            <v-icon left>fas fa-exclamation-circle</v-icon>Github Issues
-          </v-btn>
-          <v-btn
-            color="blue"
-            href="/"
-            target="blank"
-          >
-            <v-icon left>fas fa-comments</v-icon>Feedback
-          </v-btn>
-        </v-flex>
-
-        <v-flex xs10 offset-xs1 class="mt-5">
-          <div class="mb-3">Share RX100:</div>
-          <share-buttons
-            :url="hostURL"
-            title="Explore & download or watch files online"
-            desc="RX100 is a web app to explore, download or watch files online"
-          />
-        </v-flex>
-        <v-flex xs12 class="text-xs-center mt-5">
-          <a
-            href="/"
-            target="_blank"
-            title="Support us and a faster and safer web by downloading RX100"
-          >
-            <img
-              src="/img/coming-soon.png"
-              alt="Proud to support a faster, safer web for Mani5GRockers"
-            />
-          </a>
         </v-flex>
       </v-layout>
     </v-flex>
   </v-layout>
 </template>
 
-<script>
-import { mapActions } from "vuex";
-import uploadTorrentFile from "../utils/uploadTorrentFile";
-
-export default {
-  name: "home",
-  data() {
-    return {
-      loading: false,
-      query: "",
-      errors: ""
-    };
-  },
-  methods: {
-    ...mapActions(["loadTorrentInfo"]),
-    search() {
-      this.loading = true;
-      this.errors = "";
-
-      const { isEmpty, isTorrentId } = this.validateQuery;
-
-      if (isEmpty) {
-        this.errors = "Please enter torrent ID or Search Query";
-        this.loading = false;
-      } else if (isTorrentId) {
-        this.$router.push({
-          name: "explorer",
-          query: {
-            torrentId: this.query
-          }
-        });
-      } else {
-        this.$router.push({
-          name: "search",
-          query: {
-            query: this.query
-          }
-        });
-      }
-    },
-    loadDemo() {
-      this.query =
-        "magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel";
-    },
-    uploadTorrent() {
-      uploadTorrentFile.run(infoHash => {
-        this.query = infoHash;
-        window.sessionStorage["home-query"] = infoHash;
-        this.search();
-      });
-    }
-  },
-  computed: {
-    validateQuery() {
-      const query = (this.query || "").trim();
-
-      const isEmpty = query === "";
-      const isMagnet = query.match(
-        /^magnet:\?xt=urn:[a-z0-9]+:[a-f0-9]{40}(&.+=.+)*$/i
-      );
-      const isInfoHash = query.match(/^[a-f0-9]{40}$/i);
-      const isTorrentFile = query.match(/^https?:\/\/.+\.torrent$/i);
-
-      const isTorrentId = !isEmpty && (isMagnet || isInfoHash || isTorrentFile);
-
-      const isSearchQuery = !isEmpty && !isTorrentId;
-
-      return {
-        isTorrentId,
-        isMagnet,
-        isInfoHash,
-        isTorrentFile,
-        isSearchQuery,
-        isEmpty
-      };
-    }
-  },
-  watch: {
-    query(n) {
-      if (!n) n = "";
-      if (n.indexOf("%3A") !== -1 || n.indexOf("%2F") !== -1)
-        this.query = decodeURI(
-          n
-            .split("%3A")
-            .join(":")
-            .split("%2F")
-            .join("/")
-        );
-      window.sessionStorage["home-query"] = n;
-    }
-  },
-  created() {
-    document.title = "RX100";
-    const { query } = this.$route.query;
-    if (query) {
-      this.query = query;
-      this.search();
-    }
-  },
-  mounted() {
-    this.query =
-      window.sessionStorage["home-query"] || this.$route.query.query || "";
-  }
-};
-</script>
 
 <style scoped>
 .v-messages {
